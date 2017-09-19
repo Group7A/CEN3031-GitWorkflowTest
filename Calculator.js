6bbd727b4f6e890271018246e1fd99b1d84b1fd3
jmcacioppo <jmcacioppo0531@gmail.com>
2017-09-19 14:15:58 -0400

    Implemented subtract in js

--- Calculator.js
+++ Calculator.js
@@ -17,3 +17,9 @@ function divide(){
 	
 }
 		
+
+function subtract() {
+	var v1=parseInt(document.getElementById("input1").value);
+	var v2=parseInt(document.getElementById("input2").value);
+	alert("Result= " + (v1-v2).toString());
+}
